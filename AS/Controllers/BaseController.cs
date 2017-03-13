d765a22971137c35ad8f709776ae07b748d81e06
Minh Nguyen <minh.nguyen@tpssoft.com>
2017-03-13 07:35:22 +0700

    1

---
+++ AS/Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+
+namespace AS
+{
+    public class BaseController : ApiController
+    {
+        public const string Action = "[action]";
+        public const string Controller = "[controller]";
+    }
+
+    public class AuthController: BaseController
+    {
+
+    }
+}
